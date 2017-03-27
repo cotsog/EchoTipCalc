0c87c00ed2584a677b72e69e59b1877b6576d965
yingtao <guoyingtao@outlook.com>
2017-03-27 16:03:55 -0400

    Let's go
    The first commit.

---
+++ EchoTipCalc/EchoTipCalc.h
@@ -0,0 +1,19 @@
+//
+//  EchoTipCalc.h
+//  EchoTipCalc
+//
+//  Created by Echo on 2017/3/27.
+//  Copyright © 2017年 Echo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EchoTipCalc.
+FOUNDATION_EXPORT double EchoTipCalcVersionNumber;
+
+//! Project version string for EchoTipCalc.
+FOUNDATION_EXPORT const unsigned char EchoTipCalcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EchoTipCalc/PublicHeader.h>
+
+
