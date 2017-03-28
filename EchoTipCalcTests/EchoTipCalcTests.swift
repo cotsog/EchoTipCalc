da12b364aa770b61072975bf0265b4116baf0c31
guoyingtao <guoyingtao@outlook.com>
2017-03-28 19:48:47 -0400

    test

--- EchoTipCalcTests/EchoTipCalcTests.swift
+++ EchoTipCalcTests/EchoTipCalcTests.swift
@@ -35,9 +35,9 @@ class EchoTipCalcTests: XCTestCase {
         
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
     }
     
 }
